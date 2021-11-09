1fa93a1a43b2063d18a349b359fb85c927e286a8
Van Hung <vanhungnguyenbku@gmail.com>
2021-11-10 01:15:43 +0700

    .

--- src/pages/Catalog.js
+++ src/pages/Catalog.js
@@ -1,12 +1,24 @@
 import React from 'react'
 import { useParams } from 'react-router'
+import PageHeader from '../components/page-header/PageHeader'
+import { category as cate } from '../api/tmdbApi'
+import MovieGrid from '../components/movie-grid/MovieGrid'
 
-const Catalog = () => {
+const Catalog = (props) => {
 	const { category } = useParams()
 
-	console.log(category)
-
-	return <div>Catalog</div>
+	return (
+		<>
+			<PageHeader>
+				{category === cate.movie ? 'Movies' : 'TV Series'}
+			</PageHeader>
+			<div className='container'>
+				<div className='section mb-3'>
+					<MovieGrid category={category} />
+				</div>
+			</div>
+		</>
+	)
 }
 
 export default Catalog
